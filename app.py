d876bf3e8a743a95e0513d365abdd3f465a9efbe
zxc0711 <136018003+zxc0711@users.noreply.github.com>
2025-05-31 01:19:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -587,4 +587,7 @@ def update_status(index):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
